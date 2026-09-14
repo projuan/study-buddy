5d824025b23731340d7af904218cccaaf57b3670
juanocrela <juanocrela03@gmail.com>
2026-09-13 22:49:14 -0600

    Extracting leafs function done

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ answer = note_info()
 new_tree = extract_knowledge_tree(answer)
 
 
-#getting the leafs and recording them
+#getting the leafs 
 leafs = {}
 def extracting_leafs():
  for b in new_tree.branch:
    for l in b.leaf:
      leafs[l.name] = l.description
 
-     
+
+# creating an assigning a question for every leaf
+def question_assignment(l:dict) -> dict    
 extracting_leafs()
 print(leafs)
 
