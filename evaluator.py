f604b5d4a6e35cc3c95ba79b1bee403a0486d9a3
juanocrela <juanocrela03@gmail.com>
2026-09-18 00:49:31 -0600

    test deleted

--- evaluator.py
+++ evaluator.py
@@ -22,8 +22,3 @@ def evaluator(leaf:Leaf, question:str, answer:str) -> Grade:
     return message.parsed_output
 
 
-new_leaf = Leaf(name="loops" , description="A loop is a programming control flow structure that repeatedly executes a specific block of code as long as a specified condition is met. Instead of manually writing the same instructions over and over again, a loop allows you to write the code once and instruct the computer to run it multiple times, keeping your code clean and efficient.")
-new_question = quiz(new_leaf)
-new_answer = "a loop is the way we can make something repeat itself multiple times"
-graded = evaluator(new_leaf,new_question,new_answer)
-print(graded)
